a1c71c62fb3c634403115d36392b9c94cec20aba
James Johnson <jtaylorjohnson@gmail.com>
2020-03-26 23:30:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.include?(cheese_types)
-  
-  snacks.find { |snack| snack = "#{cheese_types}"}
+  if snacks.include?(cheese_types)
+    snacks.find{ |snack| snack = "#{cheese_types}"}
 end
+
